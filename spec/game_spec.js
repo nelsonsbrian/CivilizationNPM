17d7f6256ad3b70260de8addb76906a30a573bd4
Brian Nelson and Skye Nguyen <skye@>
2018-10-15 15:14:38 -0700

    add spec for getPlayer method

--- spec/game_spec.js
+++ spec/game_spec.js
@@ -21,12 +21,10 @@ describe('Game', function () {
         expect(newGame.status).toEqual(false);
     });
     it('should return game status to be false if both of the AIs are dead', function () {
-        newGame.playersArr.forEach(function(player)
-        {
-            if (player != newGame.mainPlayer)
-            {
+        newGame.playersArr.forEach(function (player) {
+            if (player != newGame.mainPlayer) {
                 player.population = 0;
-            } 
+            }
         });
         newGame.isGameOver();
         expect(newGame.status).toBe(false);
@@ -35,27 +33,29 @@ describe('Game', function () {
     it('should return game status to be false if the main player reaches 100 points in science', function () {
         newGame.mainPlayer.science = 100;
         newGame.isGameOver();
-        expect(newGame.status).toBe(false);        
-    });     
+        expect(newGame.status).toBe(false);
+    });
     it('should return game status to be false if player2 reaches 100 points in science', function () {
         newGame.playersArr[1].science = 100;
         newGame.isGameOver();
-        expect(newGame.status).toBe(false);        
-    });    
+        expect(newGame.status).toBe(false);
+    });
     it('should return game status to be false any player3 reaches 100 points in science', function () {
         newGame.playersArr[2].science = 100;
         newGame.isGameOver();
-        expect(newGame.status).toBe(false);        
+        expect(newGame.status).toBe(false);
     });
     it('should return game status to be false any player reaches 100 points in culture', function () {
         newGame.playersArr[2].culture = 100;
         newGame.isGameOver();
-        expect(newGame.status).toBe(false);        
-    });        
+        expect(newGame.status).toBe(false);
+    });
     it('should return game status to be false any player reaches 100 points in millitary', function () {
         newGame.playersArr[0].millitary = 100;
         newGame.isGameOver();
-        expect(newGame.status).toBe(false);        
-    });          
-
+        expect(newGame.status).toBe(false);
+    });
+    it('should return the corresponding player object given the id', function () {
+        expect(newGame.getPlayer(1).civName).toEqual(newGame.playersArr[1].civName);
+    })
 });
