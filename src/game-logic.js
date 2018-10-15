27e7743616cac3007215780fd205aa7b62f586d2
Brian Nelson and Skye Nguyen <brian@>
2018-10-15 11:15:08 -0700

    add end turn and isgameover methods to game class

--- src/game-logic.js
+++ src/game-logic.js
@@ -1,11 +1,11 @@
-import { getHeapStatistics } from "v8";
-
 export class Game {
     constructor(player1, player2, player3) //objects
     {
 
         this.status = true; //True = game is on-going
         this.turn = 0;
+        this.currentTurn = 0;
+        this.currentPlayer;
         this.turnStatus = true; //True = player's turn, false = AI's turn;
         this.playersArr = [player1, player2, player3];
         this.mainPlayer =
@@ -17,16 +17,50 @@ export class Game {
     }
     isGameOver() {
         //check if ANY player won
-
+        let remainingAI = 2;
         this.playersArr.forEach(function (player) {
-            if (player.science == 100 || player.millitary == 100 || player.culture == 100) {
+
+            if (player === this.mainPlayer) {
+                //check if main player just lost
+                if (this.mainPlayer.totalUnit == 0) {
+                    this.status = false;
+                }
+            } else if (player.science == 100 || player.millitary == 100 || player.culture == 100) {
+                this.status = false;
+            }
+
+            if (player.totalUnit == 0) {
+                remainingAI--;
+                player.dead = true;
+            }
+
+            if (remainingAI === 0) {
                 this.status = false;
             }
         });
+    }
 
-        //check if main player just lost
-        if (this.mainPlayer.totalUnit == 0) {
-            this.status = false;
+    endTurn()
+    {
+        isGameOver();
+        if (this.status === true)
+        {
+            if (this.currentTurn === this.playersArr.length - 1) {
+                this.currentTurn = 0;
+            } else {
+                this.currentTurn++;
+            }
+            this.currentPlayer = this.playersArr[this.currentTurn];
+            this.turn++;
+
+            if (this.currentPlayer.dead) {
+                this.endTurn();
+            } else if (this.currentPlayer.AI) {
+                const AITurnTaking = setTimeout(() => {
+                    //insert AI logic function call here
+                    this.endTurn();
+                } , 5000);
+            }
         }
     }
 }
