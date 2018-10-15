1267ff293c03225bfd3eb53e55b2eea753bd34c5
Brian Nelson and Skye Nguyen <brian@>
2018-10-15 14:40:17 -0700

    add test to check game status at the start of the game

--- spec/game_spec.js
+++ spec/game_spec.js
@@ -1,16 +1,19 @@
-import { Game } from "./game-logic";
+import { Game } from "../src/game-logic";
 import { Russia, China, Egypt } from "../src/player-logic";
 
-describe('Game', function() {
+describe('Game', function () {
+
     let player1 = new Russia();
     let player2 = new China();
     let player3 = new Egypt();
     player1.setMainPlayer();
     let newGame = new Game(player1, player2, player3);
-    let toWar = [player2, player3];
-    
-    it('should end game if the player is dead', function() {
-        expect().includes(120.83);
+
+    it('should return game status to be true at the start of the game', function () {
+
+
+        newGame.isGameOver();
+        expect(newGame.status).toEqual(true);
     });
- 
+
 });
